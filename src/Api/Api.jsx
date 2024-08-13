9c4500fb7b224c85e9c6cd3d57fdb0ea01245335
Bruno Xavier <bx9407@gmail.com>
2024-08-13 08:23:51 -0300

    Estrutura inicial

--- src/Api/Api.jsx
+++
@@ -1,7 +0,0 @@
-import UserPost from "./endpoints/UserPost";
-
-const Api = () => {
-    return<div><UserPost /></div>
-}
-
-export default Api;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import Api from './Api/Api'
+
 import './App.css'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Home from './Components/Home'
+import Login from './Components/Login/Login'
 
 function App() {
   
 
   return (
     <>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
