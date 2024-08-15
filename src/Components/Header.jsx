9826aeb5d0a8e6bbe24642c59159e144b7b1aa30
Bruno Xavier <bx9407@gmail.com>
2024-08-15 14:46:54 -0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from './Header.module.css'
 import { Link } from 'react-router-dom'
-import { ReactComponent as Dogs } from '../Assets/dogs.svg?React'
 
 function Header() {
   return (
